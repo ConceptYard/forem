6d7f2fb654ff215d13954807029d4723355a6557
Taylor Frees <taylor@conceptyard.com>
2014-05-27 16:17:38 -0700

    Redirecting Categories list to root

--- app/controllers/forem/categories_controller.rb
+++ app/controllers/forem/categories_controller.rb
@@ -2,5 +2,10 @@ module Forem
   class CategoriesController < Forem::ApplicationController
     helper 'forem/forums'
     load_and_authorize_resource :class => 'Forem::Category'
+
+    def index
+      redirect_to "/discussions#{'.json' if request.format == 'json'}"
+    end
+
   end
 end
