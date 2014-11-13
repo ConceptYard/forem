c6d83de93c5738dd89e54d8d7003d2f997ce9436
Robert Schmitt <forever@thelongterm.net>
2014-11-13 08:26:42 -0800

    added discussions count to forum

--- app/serializers/forem/forum_serializer.rb
+++ app/serializers/forem/forum_serializer.rb
@@ -1,5 +1,9 @@
 module Forem
   class ForumSerializer < ActiveModel::Serializer
-    attributes :id, :name, :slug, :description, :category_id, :views_count
+    attributes :id, :name, :slug, :description, :category_id, :views_count, :discussions_count
+
+    def discussions_count
+      object.posts.count
+    end
   end
 end
