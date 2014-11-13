a161866ee857ef1b3e4455f4fd56c156166d7897
Robert Schmitt <forever@thelongterm.net>
2014-11-13 08:39:12 -0800

    added last discussion user and date of last post

--- app/serializers/forem/forum_serializer.rb
+++ app/serializers/forem/forum_serializer.rb
@@ -1,9 +1,27 @@
 module Forem
   class ForumSerializer < ActiveModel::Serializer
-    attributes :id, :name, :slug, :description, :category_id, :views_count, :discussions_count
+    attributes :id, :name, :slug, :description, :category_id, :views_count, :discussions_count, :last_post_user_name, :last_post_date
 
     def discussions_count
       object.posts.count
+    end
+
+    def last_post_user_name
+      last_post = object.last_post_for(scope)
+      if last_post.nil?
+        'No Discussions'
+      else
+        scope.forem_name
+      end
+    end
+
+    def last_post_date
+      last_post = object.last_post_for(scope)
+      if last_post.nil?
+        ''
+      else
+        "#{distance_of_time_in_words(last_post.created_at, Time.now)} ago"
+      end
     end
   end
 end
