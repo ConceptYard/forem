1887c7917d36cce7cbace470e9c6d903c6ea55dd
Robert Schmitt <forever@thelongterm.net>
2014-11-13 09:27:58 -0800

    remove a method for debugging purposes

--- app/serializers/forem/forum_serializer.rb
+++ app/serializers/forem/forum_serializer.rb
@@ -1,6 +1,6 @@
 module Forem
   class ForumSerializer < ActiveModel::Serializer
-    attributes :id, :name, :slug, :description, :category_id, :views_count, :discussions_count, :last_post_user_name, :last_post_date
+    attributes :id, :name, :slug, :description, :category_id, :views_count, :discussions_count, :last_post_user_name #, :last_post_date
 
     def discussions_count
       object.posts.count
@@ -15,13 +15,13 @@ module Forem
       end
     end
 
-    def last_post_date
-      last_post = object.last_post_for(scope)
-      if last_post.nil?
-        ''
-      else
-        "#{distance_of_time_in_words(last_post.created_at, Time.now)} ago"
-      end
-    end
+    # def last_post_date
+    #   last_post = object.last_post_for(scope)
+    #   if last_post.nil?
+    #     ''
+    #   else
+    #     "#{distance_of_time_in_words(last_post.created_at, Time.now)} ago"
+    #   end
+    # end
   end
 end
